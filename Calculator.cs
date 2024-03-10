80b5da56c627a349543632fdcdcd09ea97cf24ec
Davud Aslanov <148844288+DavudAslanov@users.noreply.github.com>
2024-03-10 19:28:42 +0400

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_tapsiriq
+{
+    public class Calculator
+    {
+        public decimal Price;
+        public decimal One;
+        public decimal Two;
+        public char Symbol;
+        public Calculator(decimal a, decimal b, char c)
+        {
+            One = a;
+            Two = b;
+            Symbol = c;
+        }
+        
+        public void Calc()
+        {
+         
+            
+                if (Symbol == '+')
+                {
+                    Price += One + Two;
+                }
+                else if (Symbol == '-')
+                {
+                    if (One < Two)
+                    {
+                        Console.WriteLine("Menfi ededen musbet eded cixmaq mumkun deyil");
+                    }
+                    else
+                    {
+                        Price += One - Two;
+                    }
+                }
+                else if (Symbol == '*')
+                {
+                    Price += One * Two;
+                }
+                else if (Symbol == '/')
+                {
+                    Price += One / Two;
+                }
+                else
+                {
+                    Console.WriteLine("Zehmet Olmasa Daxil Etdiyiniz Reqemleri Yoxlayin");
+                }
+
+                Console.WriteLine("Cavabiniz:" + Price);
+                Console.ReadLine();  
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+/// Xetadan sonra yeniden davam
+using Calculator_tapsiriq;
+
+ try{
+        while (true)
+        {
+            Console.WriteLine("Zehmet Olmasa Birinci reqemi girin:");
+            decimal One = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Zehmet Olmasa Ikinci reqemi girin:");
+            decimal Two = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Zehmet olmasa Hansi Emeliyati Secmek istediynizi Bildirin\n *,+,-,/");
+            char Symbol = Convert.ToChar(Console.ReadLine());
+            Calculator calculator = new Calculator(One, Two, Symbol);
+            calculator.Calc();
+        }
+     }
+catch(DivideByZeroException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine("Bolme xetasi");
+    Console.ReadLine();
+    Console.WriteLine("Bolme xetasi");
+}
+catch(FormatException e)
+{
+    Console.WriteLine(e.StackTrace);
+    while (true)
+    {
+        Console.WriteLine("Zehmet Olmasa Birinci reqemi girin:");
+        decimal One = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Zehmet Olmasa Ikinci reqemi girin:");
+        decimal Two = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Zehmet olmasa Hansi Emeliyati Secmek istediynizi Bildirin\n *,+,-,/");
+        char Symbol = Convert.ToChar(Console.ReadLine());
+        Calculator calculator = new Calculator(One, Two, Symbol);
+        calculator.Calc();
+    }
+    Console.ReadLine();
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.ReadLine();
+}
+
